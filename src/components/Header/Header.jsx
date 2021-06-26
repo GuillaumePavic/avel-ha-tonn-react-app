f37bd22bf1bb58e5f2df9c6c213c1b79445357c4
GuillaumePavic <guillaume.pavic.56@gmail.com>
2021-06-26 16:41:30 +0200

    added components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css'
+
+const Header = () => {
+    return (
+        <header>
+            <ul>
+                <li>Marine Forecast</li>
+                <li>A propos</li>
+                <li>Contact</li>
+            </ul>
+        </header>
+    );
+};
+
+export default Header;
