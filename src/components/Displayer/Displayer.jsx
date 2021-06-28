8100e6e1b234642300b785f0776196694b47f78d
GuillaumePavic <guillaume.pavic.56@gmail.com>
2021-06-28 14:50:57 +0200

    installed sass

--- src/components/Displayer/Displayer.jsx
+++ src/components/Displayer/Displayer.jsx
@@ -5,6 +5,10 @@ const Displayer = ({ marker }) => {
     return (
         <div className="displayer">
             <h2>{marker.label}</h2>
+
+            <ul className="displayer_tabs">
+                <li>T0</li> <li>T+3</li> <li>T+6</li>
+            </ul>
         </div>
     );
 };
